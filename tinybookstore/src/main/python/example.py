89edd92271d552b1f4280e8586867073ca54f9ae
Sara Sprenkle <sprenkles@wlu.edu>
2017-09-29 09:54:58 -0400

    Better output and comments

--- tinybookstore/src/main/python/example.py
+++ tinybookstore/src/main/python/example.py
@@ -1,6 +1,13 @@
-import xmlrpclib
+"""
+Example Python XMLRPC client talking to the Java XMLRPC server
+"""
+
+import xmlrpc.client
 
 proxy = xmlrpc.client.ServerProxy("http://localhost:8888/")
 
-result = proxy.sumAndDifference(5, 6)
-print(result)
+result = proxy.sample.sumAndDifference(5, 6)
+sum = result[0]
+difference = result[1]
+print("Sum: ", sum)
+print("Difference: ", difference)
