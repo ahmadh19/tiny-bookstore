c17d44cd5a2c7578a70d5b287f2c9219eb22a4b4
cooperbaird <cooperbaird@38997-geol.ad.wlu.edu>
2017-10-15 21:34:55 -0400

    Basic client working

--- tinybookstore/src/main/python/book.py
+++ tinybookstore/src/main/python/book.py
@@ -1,14 +1,11 @@
 """
 Author: Hammad Ahmad
 File: book.py
+A Book object implementation
 """
 
-class Book(object):
-    "A Book object implementation"
-
-    # Constructor
+class Book:
     def __init__(self, array):
-        
         self.bookId = array[0]
         self.title = array[1]
         self.stockCount = array[4]
@@ -18,22 +15,9 @@ class Book(object):
     def __str__(self):
         return "Book ID: " + str(self.bookId) + ", Title: " + self.title + ", Topic: " + self.topic + ", Items in Stock: " + str(self.stockCount) + ", Price: $" + str(self.cost);
 
-    """
-    "Book ID: " + bookId + ", Title: "+ title + ", Topic: " + topic + ", Items in Stock: " + stockCount + ", Price: $" + cost;
-    public static String[] unpackBookAsArray(Book book) {
-    def __init__(self, bookId, title, stockCount, cost, topic):
-		String[] result = new String[5];
-		result[0] = Integer.toString(book.getBookId());
-		result[1] = book.getTitle();
-		result[2] = book.getTopic();
-		result[3] = Double.toString(book.getCost());
-		result[4] = Integer.toString(book.getStockCount());
-		return result;
-	}
-    """
-
-    def __main__(self):
-        b = Book([123, "TEST", "test category", 5.75, 15])
-        print(b)
-        
+def main():
+    b = Book([123, "TEST", "test category", 5.75, 15])
+    print(b)
     
+if __name__ == '__main__':
+    main()
--- tinybookstore/src/main/python/client.py
+++ tinybookstore/src/main/python/client.py
@@ -1,4 +1,51 @@
-import xmlrpc.client
+"""
+Author: Cooper Baird
+File: client.py
+XMLRPC client interface for the Tiny Bookstore
+"""
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8888/")
+from book import Book
+import xmlrpc.client, sys
 
+"""Returns an array of books based on the results."""
+def get_books(array):
+    books = []
+    num_of_books = int(len(array) / 5)
+    for i in range(0, num_of_books):
+        index = i*5
+        book = Book(array[index:index+5])
+        books.append(book)
+    return books
+
+hostname = "localhost"
+port = "8001"
+if len(sys.argv) > 1:
+    hostname = sys.argv[1]
+    port = str(sys.argv[2])
+    
+proxy = xmlrpc.client.ServerProxy("http://" + hostname + ":" + port + "/")
+
+print("WELCOME TO THE TINY BOOKSTORE!")
+while True:
+    print()
+    command = input("What do you want to do? (Search, Lookup, Buy) ").lower()
+    if command == "search":
+        topic = input("Topic: ")
+        result = proxy.frontEndServer.search(topic)
+        books = get_books(result)
+        for i in books: print(i)
+    elif command == "lookup":
+        item_num = int(input("Item number: "))
+        result = proxy.frontEndServer.lookup(item_num)
+        books = get_books(result)
+        print(books[0])
+    elif command == "buy":
+        item_num = int(input("Item number of book to buy: "))
+        result = proxy.frontEndServer.buy(item_num)
+        display = proxy.frontEndServer.lookup(item_num)
+        books = get_books(display)
+        print(books[0])
+    elif command == "":
+        break
+    
+        
