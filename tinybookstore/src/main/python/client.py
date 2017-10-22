817665632a0bcc65838c781d4bc555c5ef7e26f9
cooperbaird <cooperbaird@38997-geol.ad.wlu.edu>
2017-10-22 18:48:33 -0400

    refactored client.py into a class for testing

--- tinybookstore/src/main/python/client.py
+++ tinybookstore/src/main/python/client.py
@@ -7,57 +7,72 @@ XMLRPC client interface for the Tiny Bookstore
 from book import Book
 import xmlrpc.client, sys
 
-"""Returns an array of books based on the results."""
-def get_books(array):
-    books = []
-    num_of_books = int(len(array) / 5)
-    for i in range(num_of_books):
-        index = i*5
-        book = Book(array[index:index+5])
-        books.append(book)
-    return books
-
-hostname = "localhost"
-port = "8001"
-if len(sys.argv) > 1:
-    hostname = sys.argv[1]
-    port = str(sys.argv[2])
+class Client:
+    def __init__(self):
+        hostname = "localhost"
+        port = "8001"
+        if len(sys.argv) > 1:
+            hostname = sys.argv[1]
+            port = str(sys.argv[2])
+        self.proxy = xmlrpc.client.ServerProxy("http://" + hostname + ":" + port + "/")
     
-proxy = xmlrpc.client.ServerProxy("http://" + hostname + ":" + port + "/")
-
-print("WELCOME TO THE TINY BOOKSTORE!")
-while True:
-    print()
-    command = input("What do you want to do? (Search, Lookup, Buy) ").lower()
-    if command == "search":
+    """Returns an array of books based on the results."""
+    def get_books(self, array):
+        books = []
+        num_of_books = int(len(array) / 5)
+        for i in range(num_of_books):
+            index = i*5
+            book = Book(array[index:index+5])
+            books.append(book)
+        return books
+    
+    def search(self):
         topic = input("Topic: ")
-        result = proxy.frontEndServer.search(topic)
-        books = get_books(result)
-        if(len(books) == 0):
+        result = self.proxy.frontEndServer.search(topic)
+        books = self.get_books(result)
+        if (len(books) == 0):
             print("No books found. Please try again.")
         else:
-            for i in books: print(i)
-    elif command == "lookup":
+            for i in books:
+                print(i)
+                
+    def lookup(self):
         item_num = int(input("Item number: "))
         try:
-            result = proxy.frontEndServer.lookup(item_num)
-            books = get_books(result)
+            result = self.proxy.frontEndServer.lookup(item_num)
+            books = self.get_books(result)
             print(books[0])
         except Exception as exception:
             print("Book not found. Please try again.")
             #print("Got an exception:", exception)
-    elif command == "buy":
+            
+    def buy(self):
         item_num = int(input("Item number of book to buy: "))
         try:
-            result = proxy.frontEndServer.buy(item_num)
-            display = proxy.frontEndServer.lookup(item_num)
-            books = get_books(display)
+            result = self.proxy.frontEndServer.buy(item_num)
+            display = self.proxy.frontEndServer.lookup(item_num)
+            books = self.get_books(display)
             print("Bought book: ", books[0].title, ".")
         except Exception as exception:
             print("Got an exception:", exception)
-    elif command == "":
-        break
-    else:
-        print("Invalid command. Please try again.")
-    
+
+def main():
+    client = Client()
+    print("WELCOME TO THE TINY BOOKSTORE!")
+    while True:
+        print()
+        command = input("What do you want to do? (Search, Lookup, Buy) ").lower()
+        if command == "search":
+            client.search()
+        elif command == "lookup":
+            client.lookup()
+        elif command == "buy":
+            client.buy()
+        elif command == "":
+            break
+        else:
+            print("Invalid command. Please try again.")
+
+if __name__ == '__main__':
+    main()
         
